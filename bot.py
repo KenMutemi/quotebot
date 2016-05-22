360d23c8a79c137e2c2285cfda2e3e3a65bcd30d
kennedy <mutemikennedy@gmail.com>
2016-05-22 23:47:12 +0300

    Can now select quote categoties by clicking buttons

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import logging, sqlite3
+from telegram.ext import Updater, CommandHandler, MessageHandler, InlineQueryHandler, Filters
+from telegram import InlineQueryResultArticle, ParseMode, InputTextMessageContent, ReplyKeyboardMarkup, KeyboardButton, Emoji
+from uuid import uuid4
+import logging, sqlite3, re
 
 # Enable logging
 logging.basicConfig(
@@ -8,18 +10,43 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-# Connect to and query the sqlite database
-conn = sqlite3.connect('quoteDB')
+# Connect to the database
+
+conn = sqlite3.connect('quoteDB', check_same_thread=False)
 c = conn.cursor()
-c.execute('SELECT Quote, Quote_Category, Name FROM quotes1 ORDER BY RANDOM() LIMIT 1;');
-data = c.fetchone()
+
+def get_random_quote(index):
+    # Connect to and query the sqlite database
+    c.execute('SELECT Quote, Quote_Category, Name FROM quotes1 ORDER BY RANDOM() LIMIT 1;');
+    data = c.fetchone()
+    return data[index]
+
+def get_quote_by_category(index, args):
+    category = ''.join(args)
+    c.execute('SELECT Quote, Quote_Category, Name FROM quotes1 WHERE Quote_Category = "' + category.title() + '" ORDER BY RANDOM() LIMIT 1;');
+    data = c.fetchone()
+    return data[index]
+
+custom_keyboard = [[ KeyboardButton("Health"),
+    KeyboardButton("Wealth"),
+    KeyboardButton("Money"),
+    KeyboardButton("Women")]]
+reply_markup = ReplyKeyboardMarkup(custom_keyboard)
 
 # Define some command handlers
 def start(bot, update):
-    bot.sendMessage(update.message.chat_id, text="%s" % data[0])
+    text = "My name is The Thinker, and I'm a genius. I collect lots of great quotations from great people like me. If you would like to see some of them just type /category [category] and I will produce a random quote from my stash of wisdom."
+    bot.sendMessage(update.message.chat_id, text=text)
+
+def category(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text="Meow. Here's a list of the quote categories I have.",
+            reply_markup=reply_markup)
 
 def help(bot, update):
     bot.sendMessage(update.message.chat_id, text='Help!')
+
+def user_reply(bot, update):
+    bot.sendMessage(update.message.chat_id, text=get_quote_by_category(0, update.message.text))
 
 def echo(bot, update):
     bot.sendMessage(update.message.chat_id, text=update.message.text)
@@ -37,9 +64,10 @@ def main():
     # on command
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("category", category))
 
-    # on nocommand
-    dp.add_handler(MessageHandler([Filters.text], echo))
+    # On nocommand
+    dp.add_handler(MessageHandler([Filters.text], user_reply))
 
     # Log all errors
     dp.add_error_handler(error)
